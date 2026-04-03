b4daba218510553c2426f79794e1e139722be45a
Trianglemind PC <trianglemind@outlook.com>
2026-04-03 10:26:21 +0530

    commit by shivam

--- Program.cs
+++ Program.cs
@@ -42,6 +42,6 @@ app.MapControllerRoute(
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Scholaria}/{id?}");
 
 app.Run();
